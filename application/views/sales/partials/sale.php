dc7493e59f3e836cae83304e31e593b3828ee947
Visal <visal.pnc@gmail.com>
2014-09-05 15:42:59 +0700

    THORN Visal

--- application/views/sales/partials/sale.php
+++ application/views/sales/partials/sale.php
@@ -15,24 +15,8 @@
 				</div>
 			</div>
 		<?php endif; ?>
-		<div class="form-group <?php echo form_is_error('qty'); ?>">
-			<label for="qty" class="control-label col-sm-3">Quantity <span class="required">*</span></label>
-			<div class="col-sm-9">
-				<input type="text" name="qty" id="qty" class="form-control input-sm" value="<?php echo set_value('qty'); ?>" pattern=".{1,5}" title="Allow enter between 1 to 5 characters" required placeholder="Quantity" />
-				<?php echo form_error('qty'); ?>
-			</div>
-		</div>
-		<div class="form-group <?php echo form_is_error('unit_price'); ?>">
-			<label for="unit_price" class="control-label col-sm-3">Unit Price <span class="required">*</span></label>
-			<div class="col-sm-9">
-				<input type="text" name="unit_price" id="unit_price" class="form-control input-sm" value="<?php echo set_value('unit_price'); ?>" pattern=".{1,25}" title="Allow enter between 1 to 25 characters" required placeholder="Unit Price" />
-				<?php echo form_error('unit_price'); ?>
-			</div>
-		</div>
 	</div>
 	<div class="col-md-4">
-		<div class="thumbnail">
-			<?php echo img(IMG_PATH . 'w1.jpg'); ?>
-		</div>
+		
 	</div>
 </div>
