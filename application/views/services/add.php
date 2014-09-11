12c9635f088be7ae9f439d076e4329100a204809
Man Math <manmath4@gmail.com>
2014-09-12 03:13:03 +0700

    [TASK] Fixed and refactored some modules

--- application/views/services/add.php
+++ application/views/services/add.php
@@ -1,40 +1,34 @@
 <div class="panel-heading">
-	<h3 class="panel-title">New - Products Management</h3>
+	<h3 class="panel-title">New - <?php echo $title; ?></h3>
 </div>
 <div class="panel-body">
 	<?php
-	echo form_open('products/add', 'role="form" class="form-horizontal"');
+	echo form_open('services/add', 'role="form" class="form-horizontal"');
 	echo form_toolbar();
 	?>
 	<div class="row-fluid">
 		<div class="col-md-6">
 			<div class="panel panel-default">
 				<div class="panel-heading">
-					<h4 class="panel-title">Product Information</h4>
+					<h4 class="panel-title">Service Information</h4>
 				</div>
 				<div class="panel-body">
 					<div class="form-group <?php echo form_is_error('name'); ?>">
 						<?php echo form_label('Name <span class="required">*</span>', 'name', array('class' => 'col-md-3 control-label')); ?>
 						<div class="col-md-9">
-							<?php echo form_input('name', set_value('name'), 'class="form-control input-sm" placeholder="Name" pattern=".{3,50}" title="Allow enter between 3 to 50 characters"') . form_error('name'); ?>
+							<?php echo form_input('name', set_value('name'), 'id="name" class="form-control input-sm" placeholder="Name" pattern=".{2,50}" title="Allow enter between 2 to 50 characters"') . form_error('name'); ?>
 						</div>
 					</div>
 					<div class="form-group <?php echo form_is_error('name'); ?>">
-						<?php echo form_label('Price <span class="required">*</span>', 'price', array('class' => 'col-md-3 control-label')); ?>
+						<?php echo form_label('Price (USD) <span class="required">*</span>', 'service_price', array('class' => 'col-md-3 control-label')); ?>
 						<div class="col-md-9">
-							<?php echo form_input('price', set_value('price'), 'class="form-control input-sm" placeholder="Price" pattern=".{1,50}" title="Allow enter between 1 to 50 character(s)"') . form_error('price'); ?>
-						</div>
-					</div>
-					<div class="form-group <?php echo form_is_error('name'); ?>">
-						<?php echo form_label('Unit in Stock <span class="required">*</span>', 'unit_in_stock', array('class' => 'col-md-3 control-label')); ?>
-						<div class="col-md-9">
-							<?php echo form_input('unit_in_stock', set_value('unit_in_stock'), 'class="form-control input-sm" placeholder="Unit in stock" pattern=".{1,50}" title="Allow enter between 1 to 50 character(s)"') . form_error('unit_in_stock'); ?>
+							<?php echo form_input('price', set_value('price'), 'id="price" class="form-control input-sm" placeholder="Service Price" pattern=".{1,50}" title="Allow enter between 1 to 50 character(s)"') . form_error('price'); ?>
 						</div>
 					</div>
 					<div class="form-group">
 						<?php echo form_label('Description', 'description', array('class' => 'col-md-3 control-label')); ?>
 						<div class="col-md-9">
-							<textarea cols="30" rows="5" class="form-control input-sm" placeholder="Description"><?php echo set_value('description'); ?></textarea>
+							<?php echo form_textarea('description', set_value('description'), 'id="description" class="form-control input-sm"'); ?>
 						</div>
 					</div>
 				</div>
@@ -43,9 +37,15 @@
 		<div class="col-md-6">
 			<div class="panel panel-default">
 				<div class="panel-heading">
-					<h4 class="panel-title">Product Settings</h4>
+					<h4 class="panel-title">Service Settings</h4>
 				</div>
 				<div class="panel-body">
+					<div class="form-group">
+						<?php echo form_label('Category', 'cid', array('class' => 'col-sm-3 control-label')); ?>
+						<div class="col-sm-9">
+							<?php echo form_select('cid', $categories, 'category', set_value('cid')); ?>
+						</div>
+					</div>
 					<div class="form-group">
 						<?php echo form_label('Product status', 'status', array('class' => 'col-sm-3 control-label')); ?>
 						<div class="col-sm-9">
