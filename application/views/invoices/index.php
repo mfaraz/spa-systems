f7acaa18297d0624aaf80e4e8febd3ad150e8f41
visal thorn <visal.pnc@gmail.com>
2014-09-16 19:28:32 -0700

    THORN Visal

--- application/views/invoices/index.php
+++ application/views/invoices/index.php
@@ -20,26 +20,33 @@
 					</div>
 					<div class="panel-body">
 						<div class="form-group <?php echo form_is_error('card_id'); ?>">
-							<label for="cash_receive" class="control-label col-sm-3">Card ID <span
-									class="required">*</span></label>
+							<label for="cid" class="control-label col-sm-3">Select Type:</label>
 							<div class="col-md-9">
-								<input type="text" name="card_id" id="card_id" class="form-control input-sm" value="<?php echo set_value('card_id'); ?>" pattern=".{1,50}" title="Allow enter between 1 to 50 characters" required />
+								<select class="form-control select-sm" id="selectType" data-placeholder="Choose">
+									<option value="0" default selected="">Choose</option>
+									<option value="1">Card ID</option>
+									<option value="2" class="next">Customer Phone</option>
+								</select>
+							</div>
+						</div>
+						<div class="form-group <?php echo form_is_error('card_id'); ?>">
+							<label id="lCardId" for="cash_receive" class="control-label col-sm-3">Card ID</label>
+							<div class="col-md-9">
+								<input type="text" name="card_id" id="card_id" class="form-control input-sm" value="<?php echo set_value('card_id'); ?>" />
 								<?php echo form_error('card_id'); ?>
 							</div>
 						</div>
 						<div class="form-group <?php echo form_is_error('customer_phone'); ?>">
-							<label for="customer_phone" class="control-label col-sm-3">Customer Phone <span
-									class="required">*</span></label>
+							<label id="lCusPhone" for="customer_phone" class="control-label col-sm-3">Customer Phone </label>
 							<div class="col-md-9">
-								<input type="text" name="customer_phone" id="customer_phone" class="form-control input-sm" value="<?php echo set_value('customer_phone'); ?>" pattern=".{9,30}" title="Allow enter between 9 to 30 characters" required />
+								<input type="text" name="customer_phone" id="customer_phone" class="form-control input-sm" value="<?php echo set_value('customer_phone'); ?>" pattern=".{9,30}" title="Allow enter between 9 to 30 characters" />
 								<?php echo form_error('customer_phone'); ?>
 							</div>
 						</div>
 						<div class="form-group <?php echo form_is_error('cash_receive'); ?>">
-							<label for="cash_receive" class="control-label col-sm-3">Cash Received <span
-									class="required">*</span></label>
+							<label for="cash_receive" class="control-label col-sm-3">Cash Received </label>
 							<div class="col-md-9">
-								<input type="text" name="cash_receive" id="cash_receive" class="form-control input-sm" value="<?php echo set_value('cash_receive'); ?>" pattern=".{1,50}" title="Allow enter between 1 to 50 characters" required />
+								<input type="text" name="cash_receive" id="cash_receive" class="form-control input-sm" value="<?php echo set_value('cash_receive'); ?>" pattern=".{1,50}" title="Allow enter between 1 to 50 characters" />
 								<?php echo form_error('cash_receive'); ?>
 							</div>
 						</div>
@@ -91,6 +98,32 @@
 		?>
 		$( "#customer_phone" ).autocomplete({
 			source: availableTags
+		});
+	});
+	
+	$(document).ready(function() {
+		$('#lCardId').hide();
+		$('#card_id').hide();
+		$('#lCusPhone').show();
+		$('#customer_phone').show();
+		$("#selectType").change(function() {
+		   if($(this).val() == 0) {
+				$('#lCardId').hide();
+				$('#card_id').hide();
+				$('#lCusPhone').hide();
+				$('#customer_phone').hide();
+		   }
+		   else if($(this).val() == 1) {
+				$('#lCardId').show();
+				$('#card_id').show();
+				$('#lCusPhone').hide();
+				$('#customer_phone').hide();
+		   }else{
+				$('#lCardId').hide();
+				$('#card_id').hide();
+				$('#lCusPhone').show();
+				$('#customer_phone').show();
+		   }
 		});
 	});
 </script>
